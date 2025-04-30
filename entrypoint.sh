e5b8371286eb8d7020944e5f5265f9df3e04b8e2
Pavan <95187017+pavangajjala@users.noreply.github.com>
2025-04-30 15:59:23 +0530

    Update entrypoint.sh
    Added Footer

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,6 @@
 # VaultScan Action entrypoint script
 echo "🔍 Starting VaultScan..."
 python -m vaultscan.main --path $1 --verbose
+
+# Developed by Pavan Gajjala – https://github.com/pavangajjala
+# Licensed under Apache 2.0. Unauthorized removal of attribution is prohibited.
