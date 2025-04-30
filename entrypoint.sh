bd4b6ffd96509f1c7b696ca869b3879e05aab365
Pavan <pavankumar0177@gmail.com>
2025-04-30 21:20:17 +0530

    Removed unused entrypoint.sh (composite action inline steps used instead)

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# VaultScan Action entrypoint script
-echo "🔍 Starting VaultScan..."
-python -m vaultscan.main --path $1 --verbose
-
-# Developed by Pavan Gajjala – https://github.com/pavangajjala
-# Licensed under Apache 2.0. Unauthorized removal of attribution is prohibited.
